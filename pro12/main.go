82a86d320a4146b0615ad36d4c783a5921028db9
monsterzzz <monsterxmonster@163.com>
2019-07-25 02:07:58 +0800

    new buffer

--- pro12/main.go
+++ pro12/main.go
@@ -18,12 +18,16 @@ func main() {
 	fmt.Println(b)
 
 	//var a = "12s34567890"
-	var a = "123456789.0"
+	var a = "1234567890"
 	i, e := addS(a)
 	if e != nil {
 		log.Fatal("fail adds")
 	}
 	fmt.Println(i)
+
+	fmt.Println(compareS("ba", "abc"))
+	fmt.Println(sort("ba"))
+
 }
 
 func addS(s string) (string, error) {
@@ -50,4 +54,32 @@ func addS(s string) (string, error) {
 		b.WriteByte(byte(v))
 	}
 	return b.String(), e
+}
+
+func compareS(s1, s2 string) bool {
+	var b bool
+	b = true
+	if s1 == s2 {
+		b = false
+		return b
+	}
+	if len(s1) != len(s2) {
+		return false
+	}
+	if sort(s1) != sort(s2) {
+		return false
+	}
+	return true
+}
+
+func sort(s string) string {
+	b := []rune(s)
+	for i := 0; i < len(b)-1; i++ {
+		for j := i + 1; j < len(b); j++ {
+			if b[i] > b[j] {
+				b[i], b[j] = b[j], b[i]
+			}
+		}
+	}
+	return string(b)
 }
