8dca8c68a2efba557aa92fb17c19f4aa8d63b427
monsterzzz <monsterxmonster@163.com>
2019-08-12 23:26:09 +0800

    gin web

--- pro21/2/main.go
+++ pro21/2/main.go
@@ -12,6 +12,10 @@ func main() {
 
 	r.GET("/user0/:name/:sex", pathParam)
 	r.GET("/user1/", getQueryParam)
+	r.POST("/user2", postQueryParam)
+	r.POST("/user3", postGetParamBody)
+	r.POST("/user4", postFile)
+	r.POST("/user5", manyPostFile)
 	r.Run()
 }
 
@@ -35,4 +39,79 @@ func getQueryParam(c *gin.Context) {
 	sex := c.DefaultQuery("sex", "unknow")
 	message := fmt.Sprintf("hello,%s,%s,welcome to our %s Test!", name, sex, runtime.FuncForPC(funcName).Name())
 	c.String(200, message)
+}
+
+func postQueryParam(c *gin.Context) {
+	funcName, _, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Fatal("!")
+	}
+
+	name := c.PostForm("name")
+	//s := c.PostForm("name")
+	//fmt.Println(s)
+	sex := c.DefaultPostForm("sex", "unknow")
+	message := fmt.Sprintf("hello,%s,%s,welcome to our %s Test!", name, sex, runtime.FuncForPC(funcName).Name())
+	c.String(200, message)
+}
+
+func postGetParamBody(c *gin.Context) {
+	funcName, _, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Fatal("!")
+	}
+
+	name := c.DefaultQuery("name", "ww1")
+	//s := c.PostForm("name")
+	//fmt.Println(s)
+	sex := c.DefaultPostForm("sex", "unknow")
+	message := fmt.Sprintf("hello,%s,%s,welcome to our %s Test!", name, sex, runtime.FuncForPC(funcName).Name())
+	c.String(200, message)
+}
+
+func postFile(c *gin.Context) {
+	funcName, _, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Fatal("!")
+	}
+
+	//name := c.DefaultQuery("name","ww1")
+	////s := c.PostForm("name")
+	////fmt.Println(s)
+	//sex := c.DefaultPostForm("sex", "unknow")
+	file, _ := c.FormFile("file")
+	fmt.Println(file.Filename)
+	e := c.SaveUploadedFile(file, "pro21/2/hh.txt")
+	fmt.Println(e)
+	message := fmt.Sprintf("hello,welcome to our %s Test!,upload success!", runtime.FuncForPC(funcName).Name())
+	c.String(200, message)
+	//curl -X POST http://localhost:8080/user4 -F "file=@fileD/hello.txt" -H "Content-Type: multipart/form-data"
+}
+
+func manyPostFile(c *gin.Context) {
+	funcName, _, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Fatal("!")
+	}
+
+	//name := c.DefaultQuery("name","ww1")
+	////s := c.PostForm("name")
+	////fmt.Println(s)
+	//sex := c.DefaultPostForm("sex", "unknow")
+	form, e := c.MultipartForm()
+	fmt.Println(e)
+	files := form.File["upload[]"]
+	fmt.Println(form.File)
+	fmt.Println(len(files))
+	n := 0
+	for _, file := range files {
+		e := c.SaveUploadedFile(file, fmt.Sprintf("pro21/2/hh_many1_%d.txt", n))
+		//e:= c.SaveUploadedFile(file,"pro21/2/hh_many_1d.txt")
+		fmt.Println("sss")
+		fmt.Println(e)
+		n++
+	}
+	message := fmt.Sprintf("hello,welcome to our %s Test!,upload success!", runtime.FuncForPC(funcName).Name())
+	c.String(200, message)
+	//curl -X POST http://localhost:8080/user4 -F "file=@fileD/hello.txt" -H "Content-Type: multipart/form-data"
 }
